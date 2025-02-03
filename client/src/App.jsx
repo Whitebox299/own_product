86243f4729be85033edbac50aba836f18fa1b39a
suaib <suaib8211@gmail.com>
2025-02-04 01:34:42 +0530

    name changed

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,13 +2,17 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import LoginPage from './pages/LoginPage'
+import Register from './pages/RegisterPage'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
    <div>
-    <h1 className='text-red-500 text-'>hello</h1>
+  
+    <LoginPage/>
+    <Register/>
    </div>
   )
 }
